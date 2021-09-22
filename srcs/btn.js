8c0898c8d6633c17bbe8c3732f1eca96213eb5a0
myohStudent <myoh@student.42seoul.kr>
2021-09-22 17:03:20 +0900

    direct btns ongoing

--- srcs/btn.js
+++
@@ -1,8 +0,0 @@
-var hamnavi = document.querySelector(".ham-navi");
-var hamburger = document.querySelector(".hamburger");
-hamburger.addEventListener("click", toggleMenu);
-var e = document.getElementById('hamburger');
-e.addEventListener('click', function() {
-	if (this.className == 'on') this.classList.remove('on');
-	else this.classList.add('on');
-});
