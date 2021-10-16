25c9c5aae1621611d637f6c7c3dd409485378540
myohStudent <myoh@student.42seoul.kr>
2021-10-16 21:22:48 +0900

    registration form added

--- js/btns.js
+++ js/btns.js
@@ -1,3 +1,13 @@
+function openZipSearch() {
+  new daum.Postcode({
+    oncomplete: function(data) {
+      $('[name=zip]').val(data.zonecode); // 우편번호 (5자리)
+      $('[name=addr1]').val(data.address);
+      $('[name=addr2]').val(data.buildingName);
+    }
+  }).open();
+}
+
 function ifAllChecked(){
     /*
     if checked,
