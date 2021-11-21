c60b6ed4ce459786e073afdfdd6e6f4b86adc92f
myohStudent <myoh@student.42seoul.kr>
2021-11-21 12:33:23 +0900

    grid 작업중

--- js/video.js
+++ js/video.js
@@ -1,59 +1,54 @@
-var cardGrid = document.getElementById('video-grid');
+var cardGrid = document.getElementById('class-grid');
 
-var videoArr = [
+var classArr = [
+  {
+    "id": "0",
+    "title": "하이든&메르의 디지털음악",
+    "thumbnail": "./img/music.jpg"
+  },
   {
     "id": "1",
-    "title": "Croissants | Flour and Stone",
-    "thumbnail": "https://i.vimeocdn.com/video/600595198_390x220.webp"
+    "title": "시앙의 디자인 강의",
+    "thumbnail": "./img/design.jpg"
   },
   {
     "id": "2",
-    "title": "Perfect Mashed Potatoes and Gravy",
-    "thumbnail": "https://i.vimeocdn.com/video/604150056_390x220.webp"
+    "title": "자이로의 과학 강의",
+    "thumbnail": "./img/zyro.jpg"
   },
   {
     "id": "3",
-    "title": "The Heart of Soba",
-    "thumbnail": "https://i.vimeocdn.com/video/726986673_390x220.webp"
+    "title": "하이든&메르의 디지털음악",
+    "thumbnail": "./img/music.jpg"
   },
   {
     "id": "4",
-    "title": "Grilled Cheese 9 Ways",
-    "thumbnail": "https://i.vimeocdn.com/video/570486309_390x220.webp"
+    "title": "시앙의 디자인 강의",
+    "thumbnail": "./img/design.jpg"
   },
   {
     "id": "5",
-    "title": "Pineapple Cheesecake",
-    "thumbnail": "https://i.vimeocdn.com/video/602705517_390x220.webp"
+    "title": "자이로의 과학 강의",
+    "thumbnail": "./img/zyro.jpg"
   },
   {
     "id": "6",
-    "title": "Lemony Chicken Noodle Soup",
-    "thumbnail": "https://i.vimeocdn.com/video/537261616_390x220.jpg"
+    "title": "하이든&메르의 디지털음악",
+    "thumbnail": "./img/music.jpg"
   },
   {
     "id": "7",
-    "title": "Pumpkin Roll",
-    "thumbnail": "https://i.vimeocdn.com/video/608805594_590x332.jpg"
+    "title": "시앙의 디자인 강의",
+    "thumbnail": "./img/design.jpg"
   },
   {
     "id": "8",
-    "title": "How to Brine a Turkey",
-    "thumbnail": "https://i.vimeocdn.com/video/601730519_590x332.jpg"
-  },
-  {
-    "id": "9",
-    "title": "Stop Motion Dry-Brined Turkey Recipe",
-    "thumbnail": "https://i.vimeocdn.com/video/456852083_590x332.jpg"
-  },
-  {
-    "id": "10",
-    "title": "Butternut Squash Ravioli",
-    "thumbnail": "https://i.vimeocdn.com/video/600328152_590x332.jpg"
+    "title": "자이로의 과학 강의",
+    "thumbnail": "./img/zyro.jpg"
   }
 ]
 
-function generateVideoCard(videoData) {
+function generateClassCard(classData) {
   /* <div class="card">
                   <img src="https://i.vimeocdn.com/video/726986673_390x220.webp" alt="Heart of soba" class="img-grid" style="font-size: 24px"/>
                    <i class="fas fa-trash"></i>
@@ -62,42 +57,31 @@ function generateVideoCard(videoData) {
 
   var card = document.createElement('div');
   card.className = "card";
-  card.id = videoData.id;
+  card.id = classData.id;
   console.log(card)
   card.onclick = function() {
 
-    alert('Card Clicked for => ' + videoData.title);
+    alert('Card Clicked for => ' + classData.title);
   }
 
   var thumbnail = document.createElement('img');
-  thumbnail.src = videoData.thumbnail;
-  thumbnail.alt = videoData.title;
+  thumbnail.src = classData.thumbnail;
+  thumbnail.alt = classData.title;
   thumbnail.className = "img-grid";
-
-  var deleteIcon = document.createElement('i');
-  deleteIcon.className = "fas fa-trash delete-icon";
-  deleteIcon.onclick = function() {
-    alert('Card Clicked on Delete => ' + videoData.title);
-    var delt = document.getElementById(card.id)
-    console.log(delt)
-    delt.remove()
-  }
 
   var title = document.createElement('div');
   title.className = "text";
-  title.innerHTML = videoData.title;
+  title.innerHTML = classData.title;
 
   card.appendChild(thumbnail);
-  card.appendChild(deleteIcon);
+ 
   card.appendChild(title);
 
-
   cardGrid.appendChild(card);
-
   return card;
 }
 
 
-for(var i=0; i<videoArr.length; i++) {
-  generateVideoCard(videoArr[i]);
+for(var i=0; i<classArr.length; i++) {
+  generateClassCard(classArr[i]);
 }
