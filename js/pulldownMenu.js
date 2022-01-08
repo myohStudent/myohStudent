40e8b05b3f6d0f878180a774858279a50fecb030
myohStudent <myoh@student.42seoul.kr>
2022-01-08 18:16:15 +0900

    ongoing

--- js/pulldownMenu.js
+++ js/pulldownMenu.js
@@ -3,48 +3,48 @@
 header and pulldown menu
 */
 
-$(".m1").hover(function () {
-	$(this).css('cursor', 'pointer');
-	if($(".wow").css("display") == "none")
-	{
-	  $(".wow").css('display', 'block');
-	  $(".wow").animate({opacity:'0.9'}, 300);
-	}
-  });
-  $(".m2").hover(function () {
-	$(this).css('cursor', 'pointer');
-	if($(".wow").css("display") == "none")
-	{
-	  $(".wow").css('display', 'block');
-	  $(".wow").animate({opacity:'0.9'}, 300);
-	}
-  });
-  $(".m3").hover(function () {
-	$(this).css('cursor', 'pointer');
-	if($(".wow").css("display") == "none")
-	{
-	  $(".wow").css('display', 'block');
-	  $(".wow").animate({opacity:'0.9'}, 300);
-	}
-  });
-  $(".m7-my").hover(function () {
-	$(this).css('cursor', 'pointer');
-	if($(".wow").css("display") == "none")
-	{
-	  $(".wow").css('display', 'block');
-	  $(".wow").animate({opacity:'0.9'}, 300);
-	}
-  });
-  /* 타깃영역 제외한 부분 누르면 wow 없애기 */
-  $('body').on('click', function(e){
-	var $tgPoint = $(e.target);
-	// var $popCallBtn = $tgPoint.hasClass('nav-table');
-	var $popArea = $tgPoint.hasClass('wow');
+// $(".main-menu.menu1").hover(function () {
+// 	$(this).css('cursor', 'pointer');
+// 	if($(".m1").css("display") == "none")
+// 	{
+// 	  $(".m1").css('display', 'block');
+// 	  $(".m1").animate({opacity:'0.9'}, 300);
+// 	}
+//   });
+//   $(".m2").hover(function () {
+// 	$(this).css('cursor', 'pointer');
+// 	if($(".wow").css("display") == "none")
+// 	{
+// 	  $(".wow").css('display', 'block');
+// 	  $(".wow").animate({opacity:'0.9'}, 300);
+// 	}
+//   });
+//   $(".m3").hover(function () {
+// 	$(this).css('cursor', 'pointer');
+// 	if($(".wow").css("display") == "none")
+// 	{
+// 	  $(".wow").css('display', 'block');
+// 	  $(".wow").animate({opacity:'0.9'}, 300);
+// 	}
+//   });
+//   $(".m7-my").hover(function () {
+// 	$(this).css('cursor', 'pointer');
+// 	if($(".wow").css("display") == "none")
+// 	{
+// 	  $(".wow").css('display', 'block');
+// 	  $(".wow").animate({opacity:'0.9'}, 300);
+// 	}
+//   });
+//   /* 타깃영역 제외한 부분 누르면 wow 없애기 */
+//   $('body').on('click', function(e){
+// 	var $tgPoint = $(e.target);
+// 	// var $popCallBtn = $tgPoint.hasClass('nav-table');
+// 	var $popArea = $tgPoint.hasClass('wow');
   
-	if ( /*!$popCallBtn &&*/ !$popArea ) {
-	  $(".wow").animate({opacity:'0'}, 500);
-	  setTimeout(function(){
-	  $(".wow").css('display', 'none');  
-	  },500);
-	}
-  });
+// 	if ( /*!$popCallBtn &&*/ !$popArea ) {
+// 	  $(".wow").animate({opacity:'0'}, 500);
+// 	  setTimeout(function(){
+// 	  $(".wow").css('display', 'none');  
+// 	  },500);
+// 	}
+//   });
