5c4cba6e388cab1fae0bcdf8e4bea5eaf0cfd511
myohStudent <myoh@etu.42seoul.kr>
2022-01-18 16:26:33 +0900

    course page modifying

--- js/index.js
+++ js/index.js
@@ -4,3 +4,14 @@ $(document).ready(function(){
 		$("ul",this).stop().slideToggle("fast");
 	});
 });
+
+/* 
+교육과정페이지에서 course 클릭하기 (primary_steam.html, primary_culture.html, seconary_steam.html, secondary_culture.html)
+*/
+
+var courseArr = 4;
+for (var i = 0; i < courseArr; i++){
+	$(".courseNo" + i).click(function(){
+		window.location = 'courseMath.html';  
+	});
+}
